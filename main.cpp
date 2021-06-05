cfbfd4381212e1d469531937976e657a09769998
Manray9 <pasha@DESKTOP-QA938DA.localdomain>
2021-06-05 19:01:11 +0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << " | a b c d e f g h " << endl; 
+    cout << "1| r n b q k b n r " << endl;
+    cout << "2| p p p p p p p p " << endl;
+    cout << "3| _ _ _ _ _ _ _ _ " << endl;
+    cout << "4| _ _ _ _ _ _ _ _ " << endl;
+    cout << "5| _ _ _ _ _ _ _ _ " << endl;
+    cout << "6| _ _ _ _ _ _ _ _ " << endl;
+    cout << "7| P P P P P P P P " << endl;
+    cout << "8| R N B Q K B N R " << endl;
+}
